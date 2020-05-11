b8c10a2bc1060e26180e6f12ff9eba93bc84fbba
iNauj <juanignaciobracamonte@gmail.com>
2020-05-11 15:24:56 -0300

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedData: any;
+  encodedData: '';
+  encodeData: any;
+  login: number;
+  constructor(public barcodeCtrl: BarcodeScanner, private router: Router) { }
 
-  constructor() {}
+  goToBarcodeScan() {
+    var login;
+    login = 1;
+    if(login != 1){
+      this.router.navigate(['/login']);
+    }else{
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: false,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Por favor apunte al QR.',
+      resultDisplayDuration: 500,
+      formats: 'QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
 
+    this.barcodeCtrl.scan(options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedData = barcodeData;
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+}
+
+  goToUserMenu() {
+    this.router.navigate(['/login']);
+    }
 }
